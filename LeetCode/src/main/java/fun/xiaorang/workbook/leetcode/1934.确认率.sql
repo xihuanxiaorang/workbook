2ab30bd379151058815370db3cf7c65f3d34169c
小让 <15019474951@163.com>
2024-06-19 22:51:39 +0800

    feat(leetcode-sql): 1934.确认率

---
+++ LeetCode/src/main/java/fun/xiaorang/workbook/leetcode/1934.确认率.sql
@@ -0,0 +1,43 @@
+-- SQL Schema
+Drop table If Exists Signups;
+Drop table If Exists Confirmations;
+Create table If Not Exists Signups
+(
+    user_id    int,
+    time_stamp datetime
+);
+Create table If Not Exists Confirmations
+(
+    user_id    int,
+    time_stamp datetime,
+    action     ENUM ('confirmed','timeout')
+);
+Truncate table Signups;
+insert into Signups (user_id, time_stamp)
+values ('3', '2020-03-21 10:16:13');
+insert into Signups (user_id, time_stamp)
+values ('7', '2020-01-04 13:57:59');
+insert into Signups (user_id, time_stamp)
+values ('2', '2020-07-29 23:09:44');
+insert into Signups (user_id, time_stamp)
+values ('6', '2020-12-09 10:39:37');
+Truncate table Confirmations;
+insert into Confirmations (user_id, time_stamp, action)
+values ('3', '2021-01-06 03:30:46', 'timeout');
+insert into Confirmations (user_id, time_stamp, action)
+values ('3', '2021-07-14 14:00:00', 'timeout');
+insert into Confirmations (user_id, time_stamp, action)
+values ('7', '2021-06-12 11:57:29', 'confirmed');
+insert into Confirmations (user_id, time_stamp, action)
+values ('7', '2021-06-13 12:58:28', 'confirmed');
+insert into Confirmations (user_id, time_stamp, action)
+values ('7', '2021-06-14 13:59:27', 'confirmed');
+insert into Confirmations (user_id, time_stamp, action)
+values ('2', '2021-01-22 00:00:00', 'confirmed');
+insert into Confirmations (user_id, time_stamp, action)
+values ('2', '2021-02-28 23:59:59', 'timeout');
+# Write your MySQL query statement below
+SELECT s.user_id, ROUND(SUM(IF(c.action = 'confirmed', 1, 0)) / COUNT(*), 2) AS `confirmation_rate`
+FROM signups s
+         LEFT JOIN confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id;
