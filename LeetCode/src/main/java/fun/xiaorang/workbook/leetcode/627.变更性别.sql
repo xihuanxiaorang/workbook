d54e6fd19ada9364707bdf3e60c9e85dfdfa1923
小让 <15019474951@163.com>
2024-06-19 22:30:51 +0800

    feat(leetcode-sql): 627.变更性别

---
+++ LeetCode/src/main/java/fun/xiaorang/workbook/leetcode/627.变更性别.sql
@@ -0,0 +1,21 @@
+-- SQL Schema
+Drop table If Exists Salary;
+Create table If Not Exists Salary
+(
+    id     int,
+    name   varchar(100),
+    sex    char(1),
+    salary int
+);
+Truncate table Salary;
+insert into Salary (id, name, sex, salary)
+values ('1', 'A', 'm', '2500');
+insert into Salary (id, name, sex, salary)
+values ('2', 'B', 'f', '1500');
+insert into Salary (id, name, sex, salary)
+values ('3', 'C', 'm', '5500');
+insert into Salary (id, name, sex, salary)
+values ('4', 'D', 'f', '500');
+# Write your MySQL query statement below
+UPDATE salary
+SET sex = IF(sex = 'm', 'f', 'm');
