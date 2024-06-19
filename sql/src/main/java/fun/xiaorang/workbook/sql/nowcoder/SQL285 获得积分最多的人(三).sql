0b20e5a7e918f94751a64752acab8084dc8a764b
小让 <15019474951@163.com>
2024-06-19 23:53:22 +0800

    feat(nowcoder-sql): SQL285 获得积分最多的人(三)

---
+++ sql/src/main/java/fun/xiaorang/workbook/sql/nowcoder/SQL285 获得积分最多的人(三).sql
@@ -0,0 +1,41 @@
+-- SQL Schema
+drop table if exists user;
+drop table if exists grade_info;
+
+CREATE TABLE user
+(
+    id   int(4)      NOT NULL,
+    name varchar(32) NOT NULL
+);
+
+CREATE TABLE grade_info
+(
+    user_id   int(4)      NOT NULL,
+    grade_num int(4)      NOT NULL,
+    type      varchar(32) NOT NULL
+);
+
+INSERT INTO user
+VALUES (1, 'tm'),
+       (2, 'wwy'),
+       (3, 'zk'),
+       (4, 'qq'),
+       (5, 'lm');
+
+INSERT INTO grade_info
+VALUES (1, 3, 'add'),
+       (2, 3, 'add'),
+       (1, 1, 'reduce'),
+       (3, 3, 'add'),
+       (4, 3, 'add'),
+       (5, 3, 'add'),
+       (3, 1, 'reduce');
+# Write your MySQL query statement below
+SELECT id, name, grade_num
+FROM (SELECT u.id, u.name, grade_num, DENSE_RANK() OVER (ORDER BY grade_num DESC) AS `rn`
+      FROM (SELECT user_id, SUM(IF(type = 'add', grade_num, -grade_num)) AS `grade_num`
+            FROM grade_info
+            GROUP BY user_id) a
+               INNER JOIN user u ON a.user_id = u.id) b
+WHERE rn = 1
+ORDER BY id;
