7734111890d631f063b7313b5983ca0626a364ab
小让 <15019474951@163.com>
2024-06-19 22:43:52 +0800

    feat(leetcode-sql): 1693.每天的领导和合伙人

---
+++ LeetCode/src/main/java/fun/xiaorang/workbook/leetcode/1693.每天的领导和合伙人.sql
@@ -0,0 +1,34 @@
+-- SQL Schema;
+Drop table If Exists DailySales;
+Create table If Not Exists DailySales
+(
+    date_id    date,
+    make_name  varchar(20),
+    lead_id    int,
+    partner_id int
+);
+Truncate table DailySales;
+insert into DailySales (date_id, make_name, lead_id, partner_id)
+values ('2020-12-8', 'toyota', '0', '1');
+insert into DailySales (date_id, make_name, lead_id, partner_id)
+values ('2020-12-8', 'toyota', '1', '0');
+insert into DailySales (date_id, make_name, lead_id, partner_id)
+values ('2020-12-8', 'toyota', '1', '2');
+insert into DailySales (date_id, make_name, lead_id, partner_id)
+values ('2020-12-7', 'toyota', '0', '2');
+insert into DailySales (date_id, make_name, lead_id, partner_id)
+values ('2020-12-7', 'toyota', '0', '1');
+insert into DailySales (date_id, make_name, lead_id, partner_id)
+values ('2020-12-8', 'honda', '1', '2');
+insert into DailySales (date_id, make_name, lead_id, partner_id)
+values ('2020-12-8', 'honda', '2', '1');
+insert into DailySales (date_id, make_name, lead_id, partner_id)
+values ('2020-12-7', 'honda', '0', '1');
+insert into DailySales (date_id, make_name, lead_id, partner_id)
+values ('2020-12-7', 'honda', '1', '2');
+insert into DailySales (date_id, make_name, lead_id, partner_id)
+values ('2020-12-7', 'honda', '2', '1');
+# Write your MySQL query statement below
+SELECT date_id, make_name, COUNT(DISTINCT lead_id) AS `unique_leads`, COUNT(DISTINCT partner_id) AS `unique_partners`
+FROM dailysales
+GROUP BY date_id, make_name;
