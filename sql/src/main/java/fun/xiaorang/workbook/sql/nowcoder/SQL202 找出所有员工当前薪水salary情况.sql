f148678d20bd1b4509b615c57bdf13e4efee04d3
小让 <15019474951@163.com>
2024-06-19 23:30:51 +0800

    feat(nowcoder-sql): SQL202 找出所有员工当前薪水salary情况

---
+++ sql/src/main/java/fun/xiaorang/workbook/sql/nowcoder/SQL202 找出所有员工当前薪水salary情况.sql
@@ -0,0 +1,21 @@
+-- SQL Schema
+drop table if exists `salaries`;
+CREATE TABLE `salaries`
+(
+    `emp_no`    int(11) NOT NULL,
+    `salary`    int(11) NOT NULL,
+    `from_date` date    NOT NULL,
+    `to_date`   date    NOT NULL,
+    PRIMARY KEY (`emp_no`, `from_date`)
+);
+INSERT INTO salaries
+VALUES (10001, 72527, '2002-06-22', '9999-01-01');
+INSERT INTO salaries
+VALUES (10002, 72527, '2001-08-02', '9999-01-01');
+INSERT INTO salaries
+VALUES (10003, 43311, '2001-12-01', '9999-01-01');
+
+# Write your MySQL query statement below
+SELECT DISTINCT salary
+FROM salaries
+ORDER BY salary DESC;
