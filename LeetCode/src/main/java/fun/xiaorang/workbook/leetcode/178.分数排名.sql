267c08a5784fea227c3e4ad787fead582ac45a23
小让 <15019474951@163.com>
2024-06-19 22:15:39 +0800

    feat(leetcode-sql): 178.分数排名

---
+++ LeetCode/src/main/java/fun/xiaorang/workbook/leetcode/178.分数排名.sql
@@ -0,0 +1,26 @@
+-- SQL Schema
+Drop table If Exists Scores;
+Create table If Not Exists Scores
+(
+    id    int,
+    score DECIMAL(3, 2)
+);
+Truncate table Scores;
+insert into Scores (id, score)
+values ('1', '3.5');
+insert into Scores (id, score)
+values ('2', '3.65');
+insert into Scores (id, score)
+values ('3', '4.0');
+insert into Scores (id, score)
+values ('4', '3.85');
+insert into Scores (id, score)
+values ('5', '4.0');
+insert into Scores (id, score)
+values ('6', '3.65');
+
+# Write your MySQL query statement below
+SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) `rank`
+FROM Scores
+ORDER BY score DESC;
+
